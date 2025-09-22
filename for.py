04cf6a93034b2dbd6b817eb71163f4624f00103b
sofia <sofiarvp77@gmail>
2025-09-22 17:50:52 -0300

    aula BFD

---
+++ for.py
@@ -0,0 +1,29 @@
+'''vogais=['a', 'e', 'i', 'o', 'u']
+for i in vogais:
+   print (i)
+   
+for indice, valor in enumerate (vogais):
+    print (indice, valor) 
+    
+for i in range(5):
+    print(i)
+    
+#for qnd sei a qntd de repetições
+#while qnd n sei
+#ctrl+c para(stop) o código
+#interpolação smp poe % e o tipo de variavel ex s, i, f... e para imprimir vc tem q criar uma variavel com td
+#na variavel de numero tem q informar qnts casas decimais quer q mostre
+nome='gle'
+preco=10.01
+variavel='%s, o preço é %2f' % (nome, preco)
+
+#fstring
+print(f'o preco de {nome} é {preco}')
+
+#format
+'''
+i=0
+while i <5:
+    print (i)
+    i+=1 #i=1+1 (implementando i)
+    
