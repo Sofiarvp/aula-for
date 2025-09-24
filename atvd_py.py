4cb38fe3ecb2f8f765876aa6fb5fee8e3dd804e0
Sofiarvp <sofiarvp77@gmail.com>
2025-09-24 17:39:03 -0300

    Add files via upload

---
+++ atvd_py.py
@@ -0,0 +1,70 @@
+'''
+    1
+i=0
+while i<11:
+    print(i)
+    i+=1
+
+    2
+i=10
+while i<11 and i>=1:
+    print (i)
+    i-=1
+
+    3
+i=0
+while i<=20:
+    print(i)
+    i+=2
+
+    4
+
+i=1
+while i<=20 and i>1 and i:
+    print(i)
+    i+=2
+
+    5
+
+i=0
+numero=int(input('digite um numero:'))
+while i<=numero:
+    print (i)
+    i+=1
+
+    6
+i=0
+palavra=input('digite uma palavra:')
+while i<5:
+    print (palavra)
+    i+=1
+        
+        7
+i=2
+while i<=10:
+    print(i)
+    i+=2
+
+    8
+i=0
+numero=float(input('digite um numero:'))
+while i<10:
+    print(numero)
+    i+=i
+
+9
+'''
+i=0
+nome1=(input('digite um nome:'))
+nome2=(input('digite um nome:'))
+nome3=(input('digite um nome:'))
+nome4=(input('digite um nome:'))
+nome5=(input('digite um nome:'))
+while i<2:
+    lista=[nome1, nome2, nome3, nome4, nome5]
+    print (lista)
+
+
+
+
+
