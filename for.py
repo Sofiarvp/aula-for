76d141dfa3a152e7baa1648333ddf754e4ef621c
Sofiarvp <sofiarvp77@gmail.com>
2025-09-29 16:37:36 -0300

    Update for.py

--- for.py
+++ for.py
@@ -1,6 +1,10 @@
 '''vogais=['a', 'e', 'i', 'o', 'u']
 for i in vogais:
    print (i)
+
+   (laços de repetições)
+
+   (indice de elemento da lista é a posiçao q ta na lista)
    
 for indice, valor in enumerate (vogais):
     print (indice, valor) 
@@ -8,7 +12,7 @@ for indice, valor in enumerate (vogais):
 for i in range(5):
     print(i)
     
-#for qnd sei a qntd de repetições
+#for qnd sei a qntd de repetições (elementos)
 #while qnd n sei
 #ctrl+c para(stop) o código
 #interpolação smp poe % e o tipo de variavel ex s, i, f... e para imprimir vc tem q criar uma variavel com td
@@ -18,12 +22,19 @@ preco=10.01
 variavel='%s, o preço é %2f' % (nome, preco)
 
 #fstring
-print(f'o preco de {nome} é {preco}')
+print(f'o preco de {nome} é {preco}') o mais usado
 
 #format
+
 '''
 i=0
 while i <5:
     print (i)
     i+=1 #i=1+1 (implementando i)
+
+identaçao é o espaço q da com tab
+
+
+
+
     
